f3333e8f80968cf632c1aa29f412bce5bcf495ff
ThePr3 <tejo94@luukku.com>
2016-03-22 17:25:50 +0200

    Added write/read string for buffer pointer parameter

--- SpehsEngine/FileStream.cpp
+++ SpehsEngine/FileStream.cpp
@@ -5,21 +5,46 @@ using namespace boost::filesystem;
 
 namespace spehs
 {
-	static unsigned count;
 	void writeString(std::string& string, std::ofstream* stream)
 	{
-		count = string.size();
+		unsigned count = string.size();
 		stream->write((char*)&count, sizeof(count));
 		for (unsigned i = 0; i < count; i++)
 			stream->write((char*)&string[i], sizeof(char));
 	}
+	size_t writeString(std::string& string, unsigned char* buffer)
+	{
+		unsigned count = string.size();
+		memcpy(buffer, &count, sizeof(count));
+		size_t offset = sizeof(count);
+		for (unsigned i = 0; i < count; i++)
+		{
+			memcpy(&buffer[offset], &string[i], sizeof(char));
+			offset += sizeof(char);
+		}
+		return offset;
+	}
+
 	void readString(std::string& string, std::ifstream* stream)
 	{
+		unsigned count;
 		stream->read((char*)&count, sizeof(count));
-		string.resize(count);
+		string.resize(count);//Could potentially invalidate string reference??
 		for (unsigned i = 0; i < count; i++)
 			stream->read((char*)&string[i], sizeof(char));
 	}
+	size_t readString(std::string& string, unsigned char* buffer)
+	{
+		unsigned count;
+		size_t offset = 0;
+		memcpy(&count, &buffer[offset], sizeof(count));
+		offset += sizeof(count);
+		string.resize(count);//Could potentially invalidate string reference??
+		memcpy(&string[0], buffer, sizeof(char) * count);
+		offset += sizeof(char) * count;
+		return offset;
+	}
+
 	bool directoryExists(std::string& path)
 	{
 		if (exists(path))
@@ -32,6 +57,7 @@ namespace spehs
 		else
 			return false;
 	}
+
 	bool fileExists(std::string& path)
 	{
 		if (exists(path))
@@ -44,10 +70,12 @@ namespace spehs
 		else
 			return false;
 	}
+
 	bool createDirectory(std::string& path)
 	{
 		return create_directory(path);
 	}
+
 	bool verifyDirectory(std::string& path)
 	{
 		if (!spehs::directoryExists(path))
--- SpehsEngine/FileStream.h
+++ SpehsEngine/FileStream.h
@@ -5,14 +5,23 @@ namespace spehs
 {
 	/**Writes string to stream*/
 	void writeString(std::string& string, std::ofstream* stream);
+	/**Writes string to buffer. Returns number of bytes read.*/
+	size_t writeString(std::string& string, unsigned char* buffer);
+
 	/**Reads string from stream, written in writeString() format*/
 	void readString(std::string& string, std::ifstream* stream);
+	/**Reads string from buffer, written in writeString() format. Returns number of bytes read.*/
+	size_t readString(std::string& string, unsigned char* buffer);
+
 	/**Returns true if directory exists at given path*/
 	bool directoryExists(std::string& path);
+
 	/**Returns true if file exists at given path*/
 	bool fileExists(std::string& path);
+
 	/**Returns false if failed to create directory at given path*/
 	bool createDirectory(std::string& path);
+
 	/**If directory doesn't exist, creates one. If creation fails, returns false*/
 	bool verifyDirectory(std::string& path);
 }
