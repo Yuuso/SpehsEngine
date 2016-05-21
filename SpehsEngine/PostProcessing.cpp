d0505529deedb4f068bb5125948a6b40c789017a
Juuso Turunen <juusoturunen94@hotmail.com>
2016-05-21 06:38:47 +0300

    Post Processing with MSAA
    Needs a bit of cleaning up but the the code works. Doesn't take
    application data into account atm.

--- SpehsEngine/PostProcessing.cpp
+++ SpehsEngine/PostProcessing.cpp
@@ -4,14 +4,22 @@
 #include "OpenGLError.h"
 #include "Exceptions.h"
 #include "ApplicationData.h"
+#include "OpenGLError.h"
 
 #include <GL/glew.h>
 
 
+GLuint frameBufferObjectMS = 0;
+GLuint frameBufferObjectTextureIDMS = 0;
+GLuint renderBufferObjectMS = 0;
+GLuint colorBufferObjectMS = 0;
+
 GLuint frameBufferObject = 0;
-GLuint frameBufferVertexBufferObject = 0;
 GLuint frameBufferObjectTextureID = 0;
 GLuint renderBufferObject = 0;
+GLuint colorBufferObject = 0;
+
+GLuint frameBufferVertexBufferObject = 0;
 
 bool postProcessingStarted = false;
 bool usePostProcessing = false;
@@ -28,7 +36,8 @@ namespace spehs
 			{
 				if (usePostProcessing)
 				{
-					glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject);
+					glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObjectMS);
+					glEnable(GL_MULTISAMPLE);
 					postProcessingStarted = true;
 				}
 			}
@@ -36,8 +45,19 @@ namespace spehs
 			{
 				if (usePostProcessing && postProcessingStarted)
 				{
+					glBindFramebuffer(GL_READ_FRAMEBUFFER, frameBufferObjectMS);
+					glBindFramebuffer(GL_DRAW_FRAMEBUFFER, frameBufferObject);
+					glBlitFramebuffer(0, 0, applicationData->getWindowWidth(), applicationData->getWindowHeight(), 0, 0, applicationData->getWindowWidth(), applicationData->getWindowHeight(), GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
 					glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 					glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+					glDisable(GL_BLEND);
+					glDepthMask(GL_TRUE);
+					glEnable(GL_DEPTH_TEST);
+					glCullFace(GL_BACK);
+					glEnable(GL_CULL_FACE);
 
 					shaderManager->use(shaderIndex);
 					shaderManager->getShader(shaderIndex)->uniforms->textureDataID = frameBufferObjectTextureID;
@@ -48,6 +68,8 @@ namespace spehs
 					glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 					glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 					glDisableVertexAttribArray(0);
+					glBindBuffer(GL_ARRAY_BUFFER, 0);
+					shaderManager->unuse(shaderIndex);
 
 					checkOpenGLErrors(__FILE__, __LINE__);
 				}
@@ -60,7 +82,47 @@ namespace spehs
 			}
 			void enablePostProcessing()
 			{
-				//Initialize
+				//MultiSampling
+				{
+					//Texture
+					glActiveTexture(GL_TEXTURE0);
+					glGenTextures(1, &frameBufferObjectTextureIDMS);
+					glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, frameBufferObjectTextureIDMS);
+
+					glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 8, GL_RGBA, applicationData->getWindowWidth(), applicationData->getWindowHeight(), GL_TRUE);
+
+					glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+
+					//Frame Buffer
+					glGenFramebuffers(1, &frameBufferObjectMS);
+					glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObjectMS);
+
+					//Color Buffer
+					glGenRenderbuffers(1, &colorBufferObjectMS);
+					glBindRenderbuffer(GL_RENDERBUFFER, colorBufferObjectMS);
+					glRenderbufferStorageMultisample(GL_RENDERBUFFER, 8, GL_RGBA, applicationData->getWindowWidth(), applicationData->getWindowHeight());
+					glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorBufferObjectMS);
+					glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+					//Depth Render Buffer
+					glGenRenderbuffers(1, &renderBufferObjectMS);
+					glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObjectMS);
+					glRenderbufferStorageMultisample(GL_RENDERBUFFER, 8, GL_DEPTH24_STENCIL8, applicationData->getWindowWidth(), applicationData->getWindowHeight());
+					glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferObjectMS);
+					glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderBufferObjectMS);
+
+					glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, frameBufferObjectTextureIDMS, 0);
+
+					checkOpenGLErrors(__FILE__, __LINE__);
+
+					GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+					if (status != GL_FRAMEBUFFER_COMPLETE)
+					{
+						exceptions::fatalError("glCheckFramebufferStatus: error " + std::to_string(status));
+					}
+					glBindFramebuffer(GL_FRAMEBUFFER, 0);
+				}
+				
 				//Texture
 				glActiveTexture(GL_TEXTURE0);
 				glGenTextures(1, &frameBufferObjectTextureID);
@@ -72,19 +134,23 @@ namespace spehs
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, applicationData->getWindowWidth(), applicationData->getWindowHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-				glBindTexture(GL_TEXTURE_2D, 0);
 
-				//Render Buffer
-				glGenRenderbuffers(1, &renderBufferObject);
-				glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
-				glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, applicationData->getWindowWidth(), applicationData->getWindowHeight());
-				glBindRenderbuffer(GL_RENDERBUFFER, 0);
+				glBindTexture(GL_TEXTURE_2D, 0);
 
 				//Frame Buffer
 				glGenFramebuffers(1, &frameBufferObject);
 				glBindFramebuffer(GL_FRAMEBUFFER, frameBufferObject);
+
+				//Color Buffer
+				glGenRenderbuffers(1, &colorBufferObject);
+				glBindRenderbuffer(GL_RENDERBUFFER, colorBufferObject);
+				glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, applicationData->getWindowWidth(), applicationData->getWindowHeight());
+				glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorBufferObject);
+
 				glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBufferObjectTextureID, 0);
-				glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderBufferObject);
+
+				checkOpenGLErrors(__FILE__, __LINE__);
+
 				GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 				if (status != GL_FRAMEBUFFER_COMPLETE)
 				{
@@ -114,9 +180,15 @@ namespace spehs
 				if (usePostProcessing)
 				{
 					//Uninitialize
-					glDeleteRenderbuffers(1, &renderBufferObject);
+					glDeleteTextures(1, &frameBufferObjectTextureIDMS);
+					glDeleteFramebuffers(1, &frameBufferObjectMS);
+					glDeleteRenderbuffers(1, &renderBufferObjectMS);
+					glDeleteRenderbuffers(1, &colorBufferObjectMS);
+
 					glDeleteTextures(1, &frameBufferObjectTextureID);
 					glDeleteFramebuffers(1, &frameBufferObject);
+					glDeleteRenderbuffers(1, &colorBufferObject);
+
 					glDeleteBuffers(1, &frameBufferVertexBufferObject);
 
 					checkOpenGLErrors(__FILE__, __LINE__);
@@ -130,14 +202,15 @@ namespace spehs
 			}
 			void postProcessingReshape()
 			{
+				//TODO: fix
 				if (usePostProcessing)
 				{
 					glBindTexture(GL_TEXTURE_2D, frameBufferObject);
 					glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, applicationData->getWindowWidth(), applicationData->getWindowHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
 					glBindTexture(GL_TEXTURE_2D, 0);
 
-					glBindRenderbuffer(GL_RENDERBUFFER, renderBufferObject);
-					glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, applicationData->getWindowWidth(), applicationData->getWindowHeight());
+					glBindRenderbuffer(GL_RENDERBUFFER, colorBufferObject);
+					glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA8, applicationData->getWindowWidth(), applicationData->getWindowHeight());
 					glBindRenderbuffer(GL_RENDERBUFFER, 0);
 				}
 			}
