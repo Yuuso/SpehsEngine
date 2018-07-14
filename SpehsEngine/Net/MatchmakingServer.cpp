3d24d99a0b17559922de2e379b08e06736b1abd9
Teo Hiltunen <pragux@gmail.com>
2018-07-14 12:47:05 +0300

    Net: added a simple MatchmakingServer class

---
+++ SpehsEngine/Net/MatchmakingServer.cpp
@@ -0,0 +1,102 @@
+#include "stdafx.h"
+#include "SpehsEngine/Net/MatchmakingServer.h"
+
+#include <SpehsEngine/Core/Log.h>
+
+
+
+namespace se
+{
+	namespace net
+	{
+		MatchmakingServer::MatchmakingServer(const Port _port)
+			: port(_port)
+		{
+
+		}
+
+		void MatchmakingServer::update()
+		{
+			size_t acceptingClientCount = 0;
+			for (size_t i = 0; i < sockets.size(); i++)
+			{
+				sockets[i]->update();
+				if (sockets[i]->isAccepting())
+				{
+					acceptingClientCount++;
+				}
+				else if (!sockets[i]->isConnected())
+				{
+					sockets.erase(sockets.begin() + i);
+					i--;
+				}
+			}
+			if (acceptingClientCount == 0)
+			{
+				log::info("MatchMakingServer: Awaiting for a connecting client");
+				sockets.push_back(std::unique_ptr<SocketTCP>(new SocketTCP(ioService)));
+				sockets.back()->startAccepting(port, std::bind(&MatchmakingServer::onAcceptCallback, this, std::placeholders::_1));
+			}
+			else
+			{
+				SPEHS_ASSERT(acceptingClientCount == 1);
+			}
+		}
+
+		void MatchmakingServer::onAcceptCallback(SocketTCP& socketTCP)
+		{
+			for (size_t i = 0; i < sockets.size(); i++)
+			{
+				if (sockets[i].get() == &socketTCP)
+				{
+					if (socketTCP.isConnected())
+					{
+						log::info("MatchMakingServer: Client successfully connected from " + socketTCP.getRemoteEndpoint().toString());
+					}
+					else
+					{
+						log::info("MatchMakingServer: Failed to accept client");
+					}
+					return;
+				}
+			}
+		}
+
+		size_t MatchmakingServer::getConnectedSocketCount() const
+		{
+			size_t count = 0;
+			for (size_t i = 0; i < sockets.size(); i++)
+			{
+				if (sockets[i]->isConnected())
+					count++;
+			}
+			return count;
+		}
+
+		std::vector<SocketTCP*> MatchmakingServer::getConnectedSockets()
+		{
+			std::vector<SocketTCP*> connectedSockets;
+			for (size_t i = 0; i < sockets.size(); i++)
+			{
+				if (sockets[i]->isConnected())
+					connectedSockets.push_back(sockets[i].get());
+			}
+			return connectedSockets;
+		}
+
+		void MatchmakingServer::setPort(const Port& _port)
+		{
+			port = _port;
+			for (size_t i = 0; i < sockets.size(); i++)
+			{
+				if (sockets[i]->isAccepting())
+					sockets[i]->stopAccepting();
+			}
+		}
+
+		Port MatchmakingServer::getPort() const
+		{
+			return port;
+		}
+	}
+}
---
+++ SpehsEngine/Net/MatchmakingServer.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "SpehsEngine/Net/Acceptor.h"
+#include "SpehsEngine/Net/IOService.h"
+#include "SpehsEngine/Net/Port.h"
+#include "SpehsEngine/Net/SocketTCP.h"
+
+namespace se
+{
+	namespace net
+	{
+		class SocketTCP;
+		class MatchmakingServer
+		{
+		public:
+			MatchmakingServer(const Port port = 41623);
+
+			void update();
+
+			void setPort(const Port& port);
+			Port getPort() const;
+
+			size_t getConnectedSocketCount() const;
+			std::vector<SocketTCP*> getConnectedSockets();
+
+		private:
+			void onAcceptCallback(SocketTCP& socketTCP);
+
+			IOService ioService;
+			Acceptor acceptor;
+			std::vector<std::unique_ptr<SocketTCP>> sockets;
+			Port port;
+		};
+	}
+}
