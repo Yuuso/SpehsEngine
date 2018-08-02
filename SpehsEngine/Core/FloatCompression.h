555decf383059d606c4074c44736f62f7956b867
Teo Hiltunen <pragux@gmail.com>
2018-08-02 19:27:20 +0300

    Core / FloatCompression.h added

---
+++ SpehsEngine/Core/FloatCompression.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+#include "SpehsEngine/Core/WriteBuffer.h"
+#include "SpehsEngine/Core/ReadBuffer.h"
+#include "SpehsEngine/Core/Assert.h"
+
+namespace se
+{
+	template<typename UnsignedType>
+	void compress(const float value, WriteBuffer& writeBuffer, const float min, const float max)
+	{
+		static_assert(std::is_integral<UnsignedType>::value, "UnsignedType must be integral type.");
+		static_assert(std::is_unsigned<UnsignedType>::value, "UnsignedType must be unsigned type.");
+		se_assert(value >= min && value <= max)
+		const UnsignedType compressed = std::round(value * std::numeric_limits<UnsignedType>::max());
+		writeBuffer.write(compressed);
+	}
+
+	template<typename UnsignedType>
+	void uncompress(float& value, ReadBuffer& readBuffer, const float min, const float max)
+	{
+		static_assert(std::is_integral<UnsignedType>::value, "UnsignedType must be integral type.");
+		static_assert(std::is_unsigned<UnsignedType>::value, "UnsignedType must be unsigned type.");
+		UnsignedType compressed;
+		readBuffer.read(compressed);
+		value = float(compressed) / float(std::numeric_limits<UnsignedType>::max());
+		se_assert(value >= min && value <= max);
+	}
+}
