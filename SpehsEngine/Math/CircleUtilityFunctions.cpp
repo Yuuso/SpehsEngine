d488d82f6e66ce94af382db21d1135cd3bc0feef
Teo Hiltunen <pragux@gmail.com>
2018-12-10 15:50:21 +0200

    Math / checkOverlap(Circle, CIrcle): critical bug fix

--- SpehsEngine/Math/CircleUtilityFunctions.cpp
+++ SpehsEngine/Math/CircleUtilityFunctions.cpp
@@ -7,14 +7,14 @@ namespace se
 {
 	bool checkOverlap(const Circle& circle1, const Circle& circle2)
 	{
-		return glm::length(circle1.position - circle2.position) <= circle2.radius - circle1.radius;
+		return glm::length(circle1.position - circle2.position) < circle2.radius + circle1.radius;
 	}
 
 	bool checkOverlap(const Circle& circle, const std::vector<Circle>& circles)
 	{
 		for (size_t m = 0; m < circles.size(); m++)
 		{
-			if (glm::length(circle.position - circles[m].position) <= circles[m].radius - circle.radius)
+			if (checkOverlap(circle, circles[m]))
 			{
 				return true;
 			}
