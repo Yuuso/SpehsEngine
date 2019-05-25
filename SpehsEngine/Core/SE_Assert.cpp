de587efbc8898bae9c6e6d5a8c637905c9ebd212
Yuuso <juusoturunen94@hotmail.com>
2019-05-26 01:13:40 +0300

    Core: SE_Assert to use SystemMessageBox + se_assert_m
    se_assert will create a SystemMessageBox which has 3 options:
    Abort: Break execution
    Retry: Continue execution
    Ignore: Continue execution and ignore this assert in the future

--- SpehsEngine/Core/SE_Assert.cpp
+++ SpehsEngine/Core/SE_Assert.cpp
@@ -1,2 +1,33 @@
+
 #include "stdafx.h"
-#include "SpehsEngine/Core/SE_Assert.h"
+#include "SE_Assert.h"
+
+#if !defined(FINAL_RELEASE)
+#include "Log.h"
+#include "SystemMessageBox.h"
+
+
+namespace se
+{
+	bool AssertMessageBox(const std::string& _message, bool& _ignore)
+	{
+		se::log::info("Assert failed!\n" + _message, se::log::DARKRED);
+		const MessageResult result = SystemMessageBox("Assert failed!", _message, BUTTONS_ABORT_RETRY_IGNORE | ICON_WARNING);
+		switch (result)
+		{
+			default: [[fallthrough]];
+			case se::MessageResult::RESULT_ERROR: [[fallthrough]];
+			case se::MessageResult::RESULT_ABORT:
+				// ABORT: Break program execution to debug
+				return true;
+			case se::MessageResult::RESULT_RETRY:
+				// RETRY: Continue execution
+				return false;
+			case se::MessageResult::RESULT_IGNORE:
+				// IGNORE: Continue execution and ignore these asserts in the future
+				_ignore = true;
+				return false;
+		}
+	}
+}
+#endif
--- SpehsEngine/Core/SE_Assert.h
+++ SpehsEngine/Core/SE_Assert.h
@@ -1,14 +1,39 @@
+
 #pragma once
-#include "Log.h"
-#ifdef FINAL_RELEASE
-//Generate no code!
-#define se_assert(_file) ((void)0)
+
+#if defined(FINAL_RELEASE)
+	//Generate no code!
+	#define se_assert(_file) ((void)0)
 #else
-#define se_assert(expression) \
-{ \
-	if (!(expression)) { \
-		se::log::info("Assert failed.\nExpression: " #expression "\nFILE: " __FILE__ "\nLINE: " + std::to_string(__LINE__), se::log::DARKRED); \
-		__debugbreak(); \
-	} \
-}
+	#include <string>
+
+	namespace se
+	{
+		// Returns true when user want to debug
+		bool AssertMessageBox(const std::string& _message, bool& _ignore);
+	}
+
+	#define se_assert(__expression) \
+	{ \
+		if (!(__expression)) { \
+			static bool __ignore = false; \
+			if (!__ignore) { \
+				static const std::string __assertMessage("Expression: " #__expression "\nFILE: " __FILE__ "\nLINE: " + std::to_string(__LINE__)); \
+				if (se::AssertMessageBox(__assertMessage, __ignore)) \
+					__debugbreak(); \
+			} \
+		} \
+	}
+
+	#define se_assert_m(__expression, __message) \
+	{ \
+		if (!(__expression)) { \
+			static bool __ignore = false; \
+			if (!__ignore) { \
+				static const std::string __assertMessage("Expression: " #__expression "\nFILE: " __FILE__ "\nLINE: " + std::to_string(__LINE__) +  "\n" + __message); \
+				if (se::AssertMessageBox(__assertMessage, __ignore)) \
+					__debugbreak(); \
+			} \
+		} \
+	}
 #endif
