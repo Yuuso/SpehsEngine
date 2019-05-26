32e14c4f2494e549c5aa6ccfcbadd9fd1cc52140
Yuuso <juusoturunen94@hotmail.com>
2019-05-27 00:43:44 +0300

    Rendering: Print current GLContext version and line width range

--- SpehsEngine/Rendering/GLContext.cpp
+++ SpehsEngine/Rendering/GLContext.cpp
@@ -38,17 +38,17 @@ namespace se
 			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 			glClearDepth(1.0f);
 
-			//Set VSync
+			// Set VSync
 			SDL_GL_SetSwapInterval(0);//0 disable, 1 enable, -1 ???
 
-									  //Blending
-									  //glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+			// Blending
+			// glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-			//Depth
+			// Depth
 			glDepthFunc(GL_LESS);
 
-			//Face culling
+			// Face culling
 			glFrontFace(GL_CCW);
 			glCullFace(GL_BACK);
 			glEnable(GL_CULL_FACE);
@@ -57,9 +57,22 @@ namespace se
 			int MSAA;
 			SDL_GL_GetAttribute(SDL_GL_MULTISAMPLESAMPLES, &MSAA);
 			if (MSAA > 0)
+			{
+				log::info("MSAA Enabled", log::DARKGRAY);
 				glEnable(GL_MULTISAMPLE);
+			}
 
-			//Check OpenGL version
+			// Check max line width
+			GLfloat lineWidthRange[2] = { 0.0f, 0.0f };
+			glGetFloatv(GL_ALIASED_LINE_WIDTH_RANGE, lineWidthRange);
+			log::info("GL_ALIASED_LINE_WIDTH_RANGE { " + std::to_string(lineWidthRange[0]) + ", " + std::to_string(lineWidthRange[1]) + " }", log::DARKGRAY);
+
+			int minorVersion = 0, majorVersion = 0;
+			SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &minorVersion);
+			SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &majorVersion);
+			log::info("GL Context Version: " + std::to_string(majorVersion) + "." + std::to_string(minorVersion), log::DARKGRAY);
+
+			// Check OpenGL version
 			const GLubyte* glString = glGetString(GL_VERSION);
 			se_assert(glString);
 			const std::string openGLVersionString = (const char*)glGetString(GL_VERSION);
