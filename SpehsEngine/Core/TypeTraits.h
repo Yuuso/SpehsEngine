9f98c9eebf7af4f0791436ff20fb7627d13ee66f
Harcton <pragux@gmail.com>
2019-10-07 18:02:28 +0300

    Core / TypeTraits.h added. Added IsUniquePtr trait.

---
+++ SpehsEngine/Core/TypeTraits.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <memory>
+
+
+namespace se
+{
+	template<typename>
+	struct IsUniquePtr : std::false_type {};
+	template<typename T, typename D>
+	struct IsUniquePtr<std::unique_ptr<T, D>> : std::true_type {};
+}
