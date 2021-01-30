97f6104ff4b5f4649c445c6c3503cb3c1ee7953e
Teo Hiltunen <pragux@gmail.com>
2021-01-30 11:44:31 +0200

    Core / ArchiveUtilityFunctions: added a small template helper function that allows writing/reading from files more easily.

--- SpehsEngine/Core/ArchiveUtilityFunctions.h
+++ SpehsEngine/Core/ArchiveUtilityFunctions.h
@@ -1,8 +1,33 @@
 #pragma once
+
 #include "SpehsEngine/Core/Archive.h"
+
 
 namespace se
 {
-	bool writeToFile(const se::Archive& archive, const std::string& filepath);
-	bool readFromFile(se::Archive& archive, const std::string& filepath);
+	bool writeToFile(const std::string_view filepath, const se::Archive& archive);
+	bool readFromFile(const std::string_view filepath, se::Archive& archive);
+
+	template<typename T>
+	typename std::enable_if<!std::is_same<T, Archive>::value, bool>::type writeToFile(const std::string_view filepath, const T& t)
+	{
+		if (filepath.empty())
+		{
+			return false;
+		}
+
+		const se::Archive archive = t.write();
+		return writeToFile(filepath, archive);
+	}
+
+	template<typename T>
+	typename std::enable_if<!std::is_same<T, Archive>::value, bool>::type readFromFile(const std::string_view filepath, T& t)
+	{
+		se::Archive archive;
+		if (!readFromFile(filepath, archive))
+		{
+			return false;
+		}
+		return t.read(archive);
+	}
 }
