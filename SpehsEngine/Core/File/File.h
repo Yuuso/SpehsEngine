1f486b6ad404a785078563683ccb80179b5f2baf
Juuso Turunen <juusoturunen94@hotmail.com>
2021-02-27 23:40:58 +0200

    Core: File: Raw data versions for read and write

--- SpehsEngine/Core/File/File.h
+++ SpehsEngine/Core/File/File.h
@@ -12,9 +12,12 @@ namespace se
 		std::vector<uint8_t> data;
 	};
 
-	/* Opens file, retrieves file contents and closes file. Returns false on failure. */
-	bool readFile(File& file, const std::string_view path);
+	// Opens file, retrieves file contents and closes file. Returns false on failure.
+	bool readFile(File& _file);
+	bool readFile(File& _file, const std::string& _path);
+	bool readFile(const std::string& _path, uint8_t* _data, const size_t _dataSize);
 
-	/* Opens file and truncates previous contents with the given data. */
-	bool writeFile(const File& file);
+	// Opens file and truncates previous contents with the given data.
+	bool writeFile(const File& _file);
+	bool writeFile(const std::string& _path, const uint8_t* _data, const size_t _dataSize);
 }
