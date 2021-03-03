08c5498288621492b5b0cd80c2947a0d146b2498
Teo Hiltunen <pragux@gmail.com>
2021-03-03 18:44:09 +0200

    Debug / ConnectionManagerVisualizer: better visualization

--- SpehsEngine/Debug/ConnectionManagerVisualizer.cpp
+++ SpehsEngine/Debug/ConnectionManagerVisualizer.cpp
@@ -48,6 +48,9 @@ namespace se
 				{
 					if (ImGui::CollapsingHeader((connection->debugEndpoint + " \"" + connection->debugName + "\"").c_str()))
 					{
+						const se::time::Time now = se::time::now();
+						const se::time::Time historyRecordInterval = se::time::fromSeconds(0.1f);
+						ConnectionState& connectionState = connectionStates[connection->connectionId];
 						ImGui::Text("Remote endpoint: " + connection->getRemoteEndpoint().address().to_string() + ":" + std::to_string(connection->getRemoteEndpoint().port()));
 						ImGui::Text("Connection id: " + std::to_string(connection->connectionId.value));
 						switch (connection->establishmentType)
@@ -78,13 +81,124 @@ namespace se
 						ImGui::Text("Received bytes (raw): " + std::to_string(receivedBytes.raw));
 						ImGui::Text("Received bytes (user): " + std::to_string(receivedBytes.user));
 
-						ImGui::Text("Reliable send quota: " + std::to_string(connection->getReliableSendQuota()));
-						ImGui::Text("Reliable send quota per second: " + std::to_string(connection->getSendQuotaPerSecond()));
-						ImGui::Text("Unreliable send quota: " + std::to_string(connection->getUnreliableSendQuota()));
-						ImGui::Text("Reliable stream offset send: " + std::to_string(connection->getReliableStreamOffsetSend()));
-						ImGui::Text("Reliable stream offset receive: " + std::to_string(connection->getReliableStreamOffsetReceive()));
-						ImGui::Text("Reliable unacknowledged bytes in queue: " + std::to_string(connection->getReliableUnacknowledgedBytesInQueue()));
-						ImGui::Text("Reliable acknowledged bytes in queue: " + std::to_string(connection->getReliableAcknowledgedBytesInQueue()));
+						// Reliable send
+						{
+							const uint64_t reliableSentAcknowledgedBytesInQueue = connection->getReliableSentAcknowledgedBytesInQueue();
+							ImGui::Text("Reliable acknowledged bytes in queue: " + std::to_string(reliableSentAcknowledgedBytesInQueue));
+							ImGui::Text("Reliable unacknowledged bytes in queue: " + std::to_string(connection->getReliableUnacknowledgedBytesInQueue()));
+							const uint64_t reliableStreamOffsetSend = connection->getReliableStreamOffsetSend();
+							ImGui::Text("Reliable stream offset send: " + std::to_string(reliableStreamOffsetSend));
+							const uint64_t reliableAcknowledgedBytesSend = reliableStreamOffsetSend + reliableSentAcknowledgedBytesInQueue;
+							if (now - connectionState.prevReliableBytesSent.first >= historyRecordInterval)
+							{
+								if (connectionState.reliableBytesSentHistory.isFull())
+								{
+									connectionState.reliableBytesSentHistory.popFront();
+								}
+								const uint64_t advancement = reliableAcknowledgedBytesSend - connectionState.prevReliableBytesSent.second;
+								connectionState.reliableBytesSentHistory.pushBack(float(advancement));
+								connectionState.prevReliableBytesSent.first = now;
+								connectionState.prevReliableBytesSent.second = reliableAcknowledgedBytesSend;
+							}
+							float min = std::numeric_limits<float>::max();
+							float max = 0.0f;
+							float average = 0.0f;
+							for (size_t i = 0; i < connectionState.reliableBytesSentHistory.getSize(); i++)
+							{
+								const float value = connectionState.reliableBytesSentHistory[i];
+								min = std::min(min, value);
+								max = std::max(max, value);
+								average += value;
+							}
+							average /= float(connectionState.reliableBytesSentHistory.getSize());
+							ImGui::PlotLines("Reliable byte send rate",
+								connectionState.reliableBytesSentHistory.getBuffer(),
+								int(connectionState.reliableBytesSentHistory.getSize()),
+								int(connectionState.reliableBytesSentHistory.getBeginOffset()),
+								formatString("Reliable acknowledged user bytes sent\nMax: %s/s\nMin: %s/s\nAvg: %s/s",
+									toByteString(uint64_t(max / historyRecordInterval.asSeconds())),
+									toByteString(uint64_t(min / historyRecordInterval.asSeconds())),
+									toByteString(uint64_t(average / historyRecordInterval.asSeconds()))).c_str(),
+								min, max, ImVec2(ImGui::GetWindowWidth() - ImGui::GetStyle().ScrollbarSize, 100.0f));
+						}
+
+						// Reliable receive
+						{
+							const uint64_t reliableReceivedBytesInQueue = connection->getReliableReceivedBytesInQueue();
+							ImGui::Text("Reliable received bytes in queue: " + std::to_string(reliableReceivedBytesInQueue));
+							const uint64_t reliableStreamOffsetReceive = connection->getReliableStreamOffsetReceive();
+							ImGui::Text("Reliable stream offset receive: " + std::to_string(reliableStreamOffsetReceive));
+							if (now - connectionState.prevReliableBytesReceived.first >= historyRecordInterval)
+							{
+								if (connectionState.reliableBytesReceivedHistory.isFull())
+								{
+									connectionState.reliableBytesReceivedHistory.popFront();
+								}
+								const uint64_t totalReceivedBytes = reliableStreamOffsetReceive + reliableReceivedBytesInQueue;
+								const uint64_t advancement = totalReceivedBytes - connectionState.prevReliableBytesReceived.second;
+								connectionState.reliableBytesReceivedHistory.pushBack(float(advancement));
+								connectionState.prevReliableBytesReceived.first = now;
+								connectionState.prevReliableBytesReceived.second = totalReceivedBytes;
+							}
+							float min = std::numeric_limits<float>::max();
+							float max = 0.0f;
+							float average = 0.0f;
+							for (size_t i = 0; i < connectionState.reliableBytesReceivedHistory.getSize(); i++)
+							{
+								const float value = connectionState.reliableBytesReceivedHistory[i];
+								min = std::min(min, value);
+								max = std::max(max, value);
+								average += value;
+							}
+							average /= float(connectionState.reliableBytesReceivedHistory.getSize());
+							ImGui::PlotLines("Reliable byte receive rate",
+								connectionState.reliableBytesReceivedHistory.getBuffer(),
+								int(connectionState.reliableBytesReceivedHistory.getSize()),
+								int(connectionState.reliableBytesReceivedHistory.getBeginOffset()),
+								formatString("Reliable user bytes received\nMax: %s/s\nMin: %s/s\nAvg: %s/s",
+									toByteString(uint64_t(max / historyRecordInterval.asSeconds())),
+									toByteString(uint64_t(min / historyRecordInterval.asSeconds())),
+									toByteString(uint64_t(average / historyRecordInterval.asSeconds()))).c_str(),
+								min, max, ImVec2(ImGui::GetWindowWidth() - ImGui::GetStyle().ScrollbarSize, 100.0f));
+						}
+
+						// Send quota
+						{
+							const double sendQuotaPerSecond = connection->getSendQuotaPerSecond();
+							const float multiplier = 1024.0f;
+							ImGui::Text("Buffered reliable send quota: " + std::to_string(connection->getReliableSendQuota()));
+							ImGui::Text("Buffered unreliable send quota: " + std::to_string(connection->getUnreliableSendQuota()));
+							ImGui::Text("Send quota per second: " + toByteString(uint64_t(sendQuotaPerSecond)) + "/s");
+							if (now - connectionState.prevSendQuotaPerSecondTime >= historyRecordInterval)
+							{
+								if (connectionState.sendQuotaPerSecondHistory.isFull())
+								{
+									connectionState.sendQuotaPerSecondHistory.popFront();
+								}
+								connectionState.sendQuotaPerSecondHistory.pushBack(float(sendQuotaPerSecond) / multiplier);
+								connectionState.prevSendQuotaPerSecondTime = now;
+							}
+							float min = std::numeric_limits<float>::max();
+							float max = 0.0f;
+							float average = 0.0f;
+							for (size_t i = 0; i < connectionState.sendQuotaPerSecondHistory.getSize(); i++)
+							{
+								const float value = connectionState.sendQuotaPerSecondHistory[i] * multiplier;
+								min = std::min(min, value);
+								max = std::max(max, value);
+								average += value;
+							}
+							average /= float(connectionState.sendQuotaPerSecondHistory.getSize());
+							ImGui::PlotLines("Send quota per second",
+								connectionState.sendQuotaPerSecondHistory.getBuffer(),
+								int(connectionState.sendQuotaPerSecondHistory.getSize()),
+								int(connectionState.sendQuotaPerSecondHistory.getBeginOffset()),
+								formatString("Send quota per second\nMax: %s/s\nMin: %s/s\nAvg: %s/s",
+									toByteString(uint64_t(max)),
+									toByteString(uint64_t(min)),
+									toByteString(uint64_t(average))).c_str(),
+								min / multiplier, max / multiplier, ImVec2(ImGui::GetWindowWidth() - ImGui::GetStyle().ScrollbarSize, 100.0f));
+						}
 					}
 				}
 			}
--- SpehsEngine/Debug/ConnectionManagerVisualizer.h
+++ SpehsEngine/Debug/ConnectionManagerVisualizer.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "boost/signals2/connection.hpp"
+#include "SpehsEngine/Core/StaticRingBuffer.h"
+#include "SpehsEngine/Net/ConnectionPackets.h"
 
 namespace se
 {
@@ -30,11 +32,22 @@ namespace se
 
 		private:
 
+			struct ConnectionState
+			{
+				std::pair<se::time::Time, uint64_t> prevReliableBytesSent;
+				std::pair<se::time::Time, uint64_t> prevReliableBytesReceived;
+				se::time::Time prevSendQuotaPerSecondTime;
+				StaticRingBuffer<float, 64> reliableBytesSentHistory;
+				StaticRingBuffer<float, 64> reliableBytesReceivedHistory;
+				StaticRingBuffer<float, 64> sendQuotaPerSecondHistory;
+			};
+
 			void render();
 
 			net::ConnectionManager& connectionManager;
 			imgui::BackendWrapper& imGuiBackendWrapper;
 
+			std::unordered_map<se::net::ConnectionId, ConnectionState> connectionStates;
 			boost::signals2::scoped_connection preRenderConnection;
 		};
 	}
