a87a0e0b964b6e90e12c3fd33278ebcc93600051
Teo Hiltunen <pragux@gmail.com>
2021-07-20 17:29:44 +0300

    Input / Events, CustomEventParameters: added hash function for maps.

--- SpehsEngine/Input/CustomEventParametersUtilityFunctions.h
+++ SpehsEngine/Input/CustomEventParametersUtilityFunctions.h
@@ -1,11 +1,46 @@
 #pragma once
 
 #include "SpehsEngine/Core/Archive.h"
+#include "SpehsEngine/Core/Murmur3.h"
 #include "SpehsEngine/Input/CustomEventParameters.h"
+#include "SpehsEngine/Input/EventUtilityFunctions.h"
 
 
 namespace se
 {
 	Archive writeToArchive(const input::CustomEventParameters& customEventParameters);
 	bool readFromArchive(const Archive& archive, input::CustomEventParameters& customEventParameters);
+}
+
+namespace std
+{
+	template<> struct hash<se::input::CustomEventParameters>
+	{
+		size_t operator()(const se::input::CustomEventParameters& customEventParameters) const
+		{
+			size_t data[2];
+			data[0] = size_t(customEventParameters.eventType);
+			switch (customEventParameters.eventType)
+			{
+			case se::input::EventType::none: break;
+			case se::input::EventType::keyboardPress: data[1] = std::hash<se::input::KeyboardPressEvent>()(customEventParameters.keyboardPressEvent); break;
+			case se::input::EventType::keyboardDown: data[1] = std::hash<se::input::KeyboardDownEvent>()(customEventParameters.keyboardDownEvent); break;
+			case se::input::EventType::keyboardRelease: data[1] = std::hash<se::input::KeyboardReleaseEvent>()(customEventParameters.keyboardReleaseEvent); break;
+			case se::input::EventType::textInput: data[1] = std::hash<se::input::TextInputEvent>()(customEventParameters.textInputEvent); break;
+			case se::input::EventType::mouseButtonPress: data[1] = std::hash<se::input::MouseButtonPressEvent>()(customEventParameters.mouseButtonPressEvent); break;
+			case se::input::EventType::mouseButtonDown: data[1] = std::hash<se::input::MouseButtonDownEvent>()(customEventParameters.mouseButtonDownEvent); break;
+			case se::input::EventType::mouseButtonRelease: data[1] = std::hash<se::input::MouseButtonReleaseEvent>()(customEventParameters.mouseButtonReleaseEvent); break;
+			case se::input::EventType::mouseMotion: data[1] = std::hash<se::input::MouseMotionEvent>()(customEventParameters.mouseMotionEvent); break;
+			case se::input::EventType::mouseWheel: data[1] = std::hash<se::input::MouseWheelEvent>()(customEventParameters.mouseWheelEvent); break;
+			case se::input::EventType::mouseHover: data[1] = std::hash<se::input::MouseHoverEvent>()(customEventParameters.mouseHoverEvent); break;
+			case se::input::EventType::joystickButtonPress: data[1] = std::hash<se::input::JoystickButtonPressEvent>()(customEventParameters.joystickButtonPressEvent); break;
+			case se::input::EventType::joystickButtonDown: data[1] = std::hash<se::input::JoystickButtonDownEvent>()(customEventParameters.joystickButtonDownEvent); break;
+			case se::input::EventType::joystickButtonRelease: data[1] = std::hash<se::input::JoystickButtonReleaseEvent>()(customEventParameters.joystickButtonReleaseEvent); break;
+			case se::input::EventType::joystickAxis: data[1] = std::hash<se::input::JoystickAxisEvent>()(customEventParameters.joystickAxisEvent); break;
+			case se::input::EventType::quit: data[1] = std::hash<se::input::QuitEvent>()(customEventParameters.quitEvent); break;
+			case se::input::EventType::fileDrop: data[1] = std::hash<se::input::FileDropEvent>()(customEventParameters.fileDropEvent); break;
+			}
+			return size_t(se::murmurHash3_x86_32(data, sizeof(size_t) * 2, 0));
+		}
+	};
 }
--- SpehsEngine/Input/EventUtilityFunctions.h
+++ SpehsEngine/Input/EventUtilityFunctions.h
@@ -2,6 +2,7 @@
 
 #include "SpehsEngine/Input/Event.h"
 #include "SpehsEngine/Core/Archive.h"
+#include "SpehsEngine/Core/Murmur3.h"
 
 
 namespace se
@@ -39,4 +40,128 @@ namespace se
 	bool readFromArchive(const Archive& archive, input::JoystickAxisEvent& event);
 	bool readFromArchive(const Archive& archive, input::QuitEvent& event);
 	bool readFromArchive(const Archive& archive, input::FileDropEvent& event);
+}
+
+namespace std
+{
+	template<> struct hash<se::input::KeyboardPressEvent>
+	{
+		size_t operator()(const se::input::KeyboardPressEvent& event) const
+		{
+			return size_t(event.key);
+		}
+	};
+	template<> struct hash<se::input::KeyboardDownEvent>
+	{
+		size_t operator()(const se::input::KeyboardDownEvent& event) const
+		{
+			return size_t(event.key);
+		}
+	};
+	template<> struct hash<se::input::KeyboardReleaseEvent>
+	{
+		size_t operator()(const se::input::KeyboardReleaseEvent& event) const
+		{
+			return size_t(event.key);
+		}
+	};
+	template<> struct hash<se::input::TextInputEvent>
+	{
+		size_t operator()(const se::input::TextInputEvent& event) const
+		{
+			return size_t(se::murmurHash3_x86_32(event.buffer.data(), event.buffer.size(), 0));
+		}
+	};
+	template<> struct hash<se::input::MouseButtonPressEvent>
+	{
+		size_t operator()(const se::input::MouseButtonPressEvent& event) const
+		{
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::MouseButtonDownEvent>
+	{
+		size_t operator()(const se::input::MouseButtonDownEvent& event) const
+		{
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::MouseButtonReleaseEvent>
+	{
+		size_t operator()(const se::input::MouseButtonReleaseEvent& event) const
+		{
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::MouseMotionEvent>
+	{
+		size_t operator()(const se::input::MouseMotionEvent& event) const
+		{
+			static_assert(sizeof(event) == 24);
+			return size_t(se::murmurHash3_x86_32(&event, sizeof(event), 0));
+		}
+	};
+	template<> struct hash<se::input::MouseWheelEvent>
+	{
+		size_t operator()(const se::input::MouseWheelEvent& event) const
+		{
+			return size_t(se::murmurHash3_x86_32(&event.delta, 8, 0));
+		}
+	};
+	template<> struct hash<se::input::MouseHoverEvent>
+	{
+		size_t operator()(const se::input::MouseHoverEvent& event) const
+		{
+			return size_t(se::murmurHash3_x86_32(&event.position, 8, 0));
+		}
+	};
+	template<> struct hash<se::input::JoystickButtonPressEvent>
+	{
+		size_t operator()(const se::input::JoystickButtonPressEvent& event) const
+		{
+			se_assert(false && "TODO joystick pointer");
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::JoystickButtonDownEvent>
+	{
+		size_t operator()(const se::input::JoystickButtonDownEvent& event) const
+		{
+			se_assert(false && "TODO joystick pointer");
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::JoystickButtonReleaseEvent>
+	{
+		size_t operator()(const se::input::JoystickButtonReleaseEvent& event) const
+		{
+			se_assert(false && "TODO joystick pointer");
+			return size_t(event.button);
+		}
+	};
+	template<> struct hash<se::input::JoystickAxisEvent>
+	{
+		size_t operator()(const se::input::JoystickAxisEvent& event) const
+		{
+			se_assert(false && "TODO joystick pointer");
+			uint32_t data[2];
+			data[0] = event.axisIndex;
+			data[1] = uint32_t(event.axisMovement);
+			return size_t(se::murmurHash3_x86_32(data, 8, 0));
+		}
+	};
+	template<> struct hash<se::input::QuitEvent>
+	{
+		size_t operator()(const se::input::QuitEvent& event) const
+		{
+			return size_t(1);
+		}
+	};
+	template<> struct hash<se::input::FileDropEvent>
+	{
+		size_t operator()(const se::input::FileDropEvent& event) const
+		{
+			return size_t(se::murmurHash3_x86_32(event.filepath.data(), event.filepath.size(), 0));
+		}
+	};
 }
