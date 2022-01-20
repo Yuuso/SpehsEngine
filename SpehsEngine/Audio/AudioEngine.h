68eec3e49d63c917d9f1b71fed3c0ceea7efa5fc
Juuso Turunen <juusoturunen94@hotmail.com>
2022-01-20 22:38:55 +0200

    Audio: Remove old OpenAL code + add SoLoud deps
    Note: Win32/x86 is not going to be supported

--- SpehsEngine/Audio/AudioEngine.h
+++ SpehsEngine/Audio/AudioEngine.h
@@ -1,76 +1,13 @@
-
 #pragma once
-
-#include <vector>
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-
-
-struct ALCdevice_struct;
-struct ALCcontext_struct;
-
-typedef struct ALCdevice_struct ALCdevice;
-typedef struct ALCcontext_struct ALCcontext;
-
-typedef unsigned int ALuint;
 
 
 namespace se
 {
 	namespace audio
 	{
-		//easy debug/temp sounds
-
 		class AudioEngine
 		{
-			friend class SoundSource;
-			friend class ActiveSoundSource;
 		public:
-			struct SourceObject
-			{
-				ALuint sourceID = 0;
-				SoundSource* soundPtr = nullptr;
-			};
-
-			static void init();
-			static void uninit();
-
-			static void update();
-
-			static void setMaxSources(const unsigned int _maxSources);
-
-			static void setListenerPosition(const glm::vec2& _pos);
-			static void setListenerPosition(const glm::vec3& _pos);
-
-			static void setListenerVelocity(const glm::vec2& _vel);
-			static void setListenerVelocity(const glm::vec3& _vel);
-
-			static void setListenerUp(const glm::vec3& _up);
-			static void setListenerForward(const glm::vec3& _forward);
-
-			static void setListenerGain(const float _gain);
-
-			//These correction factors are applied to all sound and the listener positions and velocities. They are used to set the scale of the units used in positions.
-			static void setPositionCorrectionFactor(const glm::vec2& _poscor);
-			static void setPositionCorrectionFactor(const glm::vec3& _poscor);
-
-			static glm::vec2 getListenerPosition();
-			static glm::vec2 getListenerVelocity();
-			static float getListenerGain();
-
-			//Audio channels
-			/**If referencing previously non-existing channel, channel is created. A negative channel index refers to plain master channel.*/
-			static void setChannelGain(const int _channelIndex, float _gain);
-			/**If referencing previously non-existing channel, channel is created. A negative channel index refers to plain master channel.*/
-			static float getChannelGain(const int _channelIndex);
-
-			static float deltaSeconds;
-
-			static float masterVolume;
-
-		protected:
-			static void updateGain(); //Call when master volume has changed
-			static bool getFreeSource(SoundSource* _soundSource);
 		};
 	}
 }
--- SpehsEngine/Audio/AudioLib.cpp
+++ SpehsEngine/Audio/AudioLib.cpp
@@ -1,70 +1,32 @@
 #include "stdafx.h"
 #include "SpehsEngine/Audio/AudioLib.h"
 
-#include "SpehsEngine/Audio/AudioEngine.h"
-#include "SpehsEngine/Audio/AudioManager.h"
-#include "SpehsEngine/Core/CoreLib.h"
-#include "SpehsEngine/Core/Log.h"
-#include "SpehsEngine/Core/SE_Time.h"
-#include <iostream>
-#include <stdint.h>
-#include <string>
-#include <thread>
-
 
 namespace se
 {
 	namespace
 	{
-		int instanceCount = 0;
-		bool valid = false;
-		std::string version("0");
+		static int instanceCount = 0;
+		static bool valid = false;
 	}
 
-	AudioLib::AudioLib(const CoreLib& coreLib)
+	AudioLib::AudioLib()
 	{
 		instanceCount++;
 		if (!valid)
 		{
-			if (!coreLib.isValid())
-			{
-				log::error("Cannot initialize audio library, core library is invalid.");
-				return;
-			}
-
-			log::info("Current SpehsEngine audio library build: " + getVersion());
-
-			//INITIALIZATIONS
-			audio::AudioEngine::init();
-			AudioManager::init();
-
 			valid = true;
 		}
 	}
-		
 	AudioLib::~AudioLib()
 	{
 		if (--instanceCount == 0)
 		{
-			audio::AudioEngine::uninit();
-			AudioManager::uninit();
 			valid = false;
 		}
 	}
-
-	void AudioLib::update()
-	{
-		SE_SCOPE_PROFILER();
-		audio::AudioEngine::update();
-	}
-
 	bool AudioLib::isValid()
 	{
 		return valid;
-	}
-
-	std::string AudioLib::getVersion()
-	{
-		return version;
 	}
 }
