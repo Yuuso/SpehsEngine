18c3e62f289c40eb9ce0fe01439f72823f3c70d4
Teo Hiltunen <pragux@gmail.com>
2022-06-17 10:00:56 +0300

    Replaced some 4x spaces with tabs

--- SpehsEngine/Core/HasMemberFunction.h
+++ SpehsEngine/Core/HasMemberFunction.h
@@ -3,10 +3,10 @@
 template<typename T, typename Sign> \
 struct structName \
 { \
-    typedef char yes[1]; \
-    typedef char no [2]; \
-    template <typename U, U> struct type_check; \
-    template <typename _1> static yes &chk(type_check<Sign, &_1::functionName > *); \
-    template <typename   > static no  &chk(...); \
-    static bool const value = sizeof(chk<T>(0)) == sizeof(yes); \
+	typedef char yes[1]; \
+	typedef char no [2]; \
+	template <typename U, U> struct type_check; \
+	template <typename _1> static yes &chk(type_check<Sign, &_1::functionName > *); \
+	template <typename   > static no  &chk(...); \
+	static bool const value = sizeof(chk<T>(0)) == sizeof(yes); \
 }
