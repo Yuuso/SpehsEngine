cfe9d5e2b24d540b04bbc653f9ad5dd5d1f3dd8a
Teo Hiltunen <pragux@gmail.com>
2022-09-12 07:57:17 +0300

    ImGui / Utility / TextColored: added format string parameters.

--- SpehsEngine/ImGui/Utility/ImGuiUtility.h
+++ SpehsEngine/ImGui/Utility/ImGuiUtility.h
@@ -123,13 +123,15 @@ namespace ImGui
 		Text(label.c_str());
 	}
 
-	inline void TextColored(const se::Color& color, const char* const label)
+	template<typename ... Args>
+	inline void TextColored(const se::Color& color, const char* const format, Args ... args)
 	{
-		TextColored((ImVec4&)color, label);
+		TextColored((ImVec4&)color, format, args...);
 	}
-	inline void TextColored(const se::Color& color, const std::string& label)
+	template<typename ... Args>
+	inline void TextColored(const se::Color& color, const std::string& format, Args ... args)
 	{
-		TextColored((ImVec4&)color, label.c_str());
+		TextColored((ImVec4&)color, format.c_str(), args...);
 	}
 
 	inline ImVec2 SizeToScale(const se::graphics::Texture& _texture, const ImVec2 _size)
