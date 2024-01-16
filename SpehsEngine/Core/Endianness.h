7180ccceb8b7308c2e8e9174825d80baa1ad28e8
Juuso Turunen <juusoturunen94@hotmail.com>
2024-01-16 04:15:48 +0200

    Core: Endianness swapping functions

--- SpehsEngine/Core/Endianness.h
+++ SpehsEngine/Core/Endianness.h
@@ -15,4 +15,39 @@ namespace se
 	extern std::string getEndiannessAsString(const Endianness endianness);
 	extern const Endianness hostByteOrder;
 	extern const Endianness networkByteOrder;
+
+	template<typename T>
+	T swapEndianness(T _value)
+	{
+		union
+		{
+			T value;
+			uint8_t bytes[sizeof(T)];
+		} source, dest;
+		source.value = _value;
+
+		for (size_t i = 0; i < sizeof(T); i++)
+		{
+			dest.bytes[i] = source.bytes[sizeof(T) - i - 1];
+		}
+		return dest.value;
+	}
+	template<typename T>
+	T networkToHost(T value)
+	{
+		if (hostByteOrder == networkByteOrder)
+		{
+			return value;
+		}
+		return swapEndianness(value);
+	}
+	template<typename T>
+	T hostToNetwork(T value)
+	{
+		if (hostByteOrder == networkByteOrder)
+		{
+			return value;
+		}
+		return swapEndianness(value);
+	}
 }
