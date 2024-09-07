3e03fa7bc2e2ccc71d14fed74e72a3649dd0a405
Teo Hiltunen <pragux@gmail.com>
2024-09-07 19:43:25 +0300

    Physics: added forward declares and cleaned header includes.

--- SpehsEngine/Physics/3D/FrustumCollider3D.h
+++ SpehsEngine/Physics/3D/FrustumCollider3D.h
@@ -1,7 +1,4 @@
-
 #pragma once
-
-#include <glm/vec3.hpp>
 
 
 namespace se
--- SpehsEngine/Physics/PrecompiledInclude.h
+++ SpehsEngine/Physics/PrecompiledInclude.h
@@ -1,1 +1,16 @@
 #pragma once
+
+
+namespace se
+{
+	namespace physics
+	{
+		class AABBCollider3D;
+		class Collision3D;
+		class FrustumCollider3D;
+		class Ray3D;
+		class SphereCollider3D;
+		struct CollisionResults;
+		struct Projection;
+	}
+}
